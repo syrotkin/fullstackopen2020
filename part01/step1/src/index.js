725532e1e352a912372501f030fefc9a4d10506a
syrotkin <syrotkin@gmail.com>
2021-03-11 21:23:53 +0100

    after wrong re-rendering

--- part01/step1/src/index.js
+++ part01/step1/src/index.js
@@ -1,89 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 
-const Header = (props) => {
-  return <h1>{props.course}</h1>;
+let counter = 1;
+
+const refresh = () => {
+  ReactDOM.render(<App counter={counter} />, document.getElementById("root"));
 };
 
-const Part = (props) => (
-  <p>
-    {props.part} {props.exercises}
-  </p>
-);
+refresh();
 
-const Content = (props) => {
-  const { parts } = props;
-  return (
-    <div>
-      <Part part={parts[0].name} exercises={parts[0].exercises} />
-      <Part part={parts[1].name} exercises={parts[1].exercises} />
-      <Part part={parts[2].name} exercises={parts[2].exercises} />
-    </div>
-  );
-};
+let interval = setInterval(() => {
+  counter += 1;
+  refresh();
+}, 1000);
 
-const Total = (props) => {
-  const { parts } = props;
-  return (
-    <p>
-      Number of exercises{" "}
-      {parts[0].exercises + parts[1].exercises + parts[2].exercises}
-    </p>
-  );
-};
-
-const AppOld = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
-  };
-
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
-};
-
-const Hello = ({ name, age }) => {
-  const bornYear = () => new Date().getFullYear() - age;
-
-  return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So, you were probably born in {bornYear()}.</p>
-    </div>
-  );
-};
-
-const App = () => {
-  const name = "Peter";
-  const age = 10;
-
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-    </div>
-  );
-};
-
-ReactDOM.render(<App />, document.getElementById("root"));
+setTimeout(() => { clearInterval(interval);}, 5000);
