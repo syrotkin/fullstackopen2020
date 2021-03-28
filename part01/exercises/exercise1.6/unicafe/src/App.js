c2e69953b8dcfd2e4e4d392dd65f101f257e7c9e
syrotkin <syrotkin@gmail.com>
2021-03-28 17:47:40 +0200

    end of exercise 1.8

--- part01/exercises/exercise1.6/unicafe/src/App.js
+++ part01/exercises/exercise1.6/unicafe/src/App.js
@@ -5,28 +5,19 @@ const Button = ({ text, handleClick }) => {
   return <button onClick={handleClick}>{text}</button>;
 };
 
-const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-
+const Statistics = ({good, neutral, bad}) => {
   const getAverageScore = () => {
-    return (good - bad)/countAllFeedback();
+    return (good - bad) / countAllFeedback();
   };
 
   const getPercentagePositive = () => {
-    return (good/countAllFeedback()) * 100
-
+    return (good / countAllFeedback()) * 100;
   };
- 
-  const countAllFeedback = ()=> good + neutral + bad;
+
+  const countAllFeedback = () => good + neutral + bad;
 
   return (
     <div>
-      <h3>give feedback</h3>
-      <Button text="good" handleClick={() => setGood(good + 1)} />
-      <Button text="neutral" handleClick={() => setNeutral(neutral + 1)} />
-      <Button text="bad" handleClick={() => setBad(bad + 1)} />
       <h3>statistics</h3>
       <p>good: {good}</p>
       <p>neutral: {neutral}</p>
@@ -34,6 +25,22 @@ const App = () => {
       <p>all: {countAllFeedback()}</p>
       <p>average: {getAverageScore()}</p>
       <p>positive: {`${getPercentagePositive()} %`}</p>
+    </div>
+  );
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <h3>give feedback</h3>
+      <Button text="good" handleClick={() => setGood(good + 1)} />
+      <Button text="neutral" handleClick={() => setNeutral(neutral + 1)} />
+      <Button text="bad" handleClick={() => setBad(bad + 1)} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
