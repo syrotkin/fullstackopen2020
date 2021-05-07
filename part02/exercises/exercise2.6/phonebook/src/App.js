1de04a6a4ae4646c0eddadadc963eb8c3ea841cc
syrotkin <syrotkin@gmail.com>
2021-05-07 23:00:04 +0200

    end of exercise 2.11

--- part02/exercises/exercise2.6/phonebook/src/App.js
+++ part02/exercises/exercise2.6/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 
 const Person = ({ person }) => {
@@ -43,15 +44,18 @@ const AddPersonForm = (props) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filterValue, setFilterValue] = useState("");
+
+
+useEffect(() => {
+  axios
+  .get('http://localhost:3001/persons')
+  .then(response  => 
+    setPersons(response.data));
+}, [])
 
   const handleNewNameChange = (event) => {
     event.preventDefault();
