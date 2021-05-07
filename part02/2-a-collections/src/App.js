9c5232e496009b8ee299ea940b191a189f50c03b
syrotkin <syrotkin@gmail.com>
2021-05-07 22:37:39 +0200

    before exercise 2.11

--- part02/2-a-collections/src/App.js
+++ part02/2-a-collections/src/App.js
@@ -1,10 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Note from "./components/Note";
+import axios from 'axios';
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes);
+// npx json-server --port 3001 --watch db.json
+const App = () => {
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState('');
   const [showAll, setShowAll] = useState(true);
+
+  const hook = () => {
+    console.log('effect');
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled');
+        setNotes(response.data);
+      });
+  };
+
+  useEffect(hook, []); // second parameter - how often is the effect run; empty array => run only after 1st render
+
+  console.log('render', notes.length, 'notes');
 
   const addNote = (event) => {
     event.preventDefault();
@@ -45,7 +61,6 @@ const App = (props) => {
           <button type="submit">Save</button>
       </form>
       <button onClick={toggleImportant}>show {showAll ? 'important' : 'all'}</button>
-      
     </div>
   );
 };
