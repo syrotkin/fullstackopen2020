2b1b40ef19d3a4fc3cde8bcaf2e50d92a06efbea
syrotkin <syrotkin@gmail.com>
2021-05-11 21:34:26 +0200

    add only countries to state (exercise 2.12)

--- part02/exercises/exercise2.12/countries/src/App.js
+++ part02/exercises/exercise2.12/countries/src/App.js
@@ -2,17 +2,9 @@ import React from "react";
 import { useState } from "react";
 import axios from "axios";
 
-const CountryMessage = ({ count }) => {
-  if (count > 10) {
-    return <div>Too many matches, specify another filter</div>;
-  }
-
-  return null;
-};
-
 const CountryList = ({ countries }) => {
   const countryList = countries.map((country) => (
-    <div key={country}>{country}</div>
+    <div key={country.name}>{country.name}</div>
   ));
   return <div>{countryList}</div>;
 };
@@ -45,17 +37,26 @@ const Country = ({ country }) => {
       <div>capital {country.capital}</div>
       <div>population {country.population}</div>
       <Languages country={country} />
-      <img src={country.flag} width="200" height="100"  />
+      <img src={country.flag} width="200" height="100" alt="" />
     </div>
   );
 };
 
+const Countries = ({countries}) => {
+  if (countries.length > 10) {
+    return <div>Too many matches, specify another filter</div>;
+  }
+
+  if (countries.length === 1) {
+    return <Country country={countries[0]} />;
+  }
+
+  return <CountryList countries={countries} />;
+};
+
 const App = () => {
   const [searchText, setSearchText] = useState("");
-  // TODO: you can only setCountries and do the logic in the specific components
-  const [countryCount, setCountryCount] = useState(0);
   const [countries, setCountries] = useState([]);
-  const [country, setCountry] = useState(null);
 
   const handleTextChange = (event) => {
     event.preventDefault();
@@ -68,19 +69,7 @@ const App = () => {
       if (response.data) {
         const countries = response.data;
         console.log(countries);
-        setCountryCount(countries.length);
-        if (countries.length > 10) {
-          setCountries([]);
-          setCountry(null);
-        } else if (countries.length <= 10 && countries.length > 1) {
-          const countryNames = countries.map((c) => c.name);
-          setCountries(countryNames);
-          setCountry(null);
-        } else if (countries.length === 1) {
-          setCountries([]);
-          setCountry(countries[0]);
-          console.log(country);
-        }
+        setCountries(countries);
       }
     });
     // TODO: what to do on error?
@@ -90,9 +79,7 @@ const App = () => {
     <div>
       <span>find countries </span>
       <input type="text" onChange={handleTextChange} value={searchText} />
-      <CountryMessage count={countryCount} />
-      <CountryList countries={countries} />
-      <Country country={country} />
+      <Countries countries={countries} />
     </div>
   );
 };
