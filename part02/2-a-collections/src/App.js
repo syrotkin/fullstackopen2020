fb5441a2721dd1375cdb5b0388747b6d47d6a47d
syrotkin <syrotkin@gmail.com>
2022-12-28 15:00:46 +0100

    catch error

--- part02/2-a-collections/src/App.js
+++ part02/2-a-collections/src/App.js
@@ -50,13 +50,18 @@ const App = () => {
 
   const notesToShow = showAll ? notes : notes.filter(n => n.important);
 
-  const toggleImportance = (id) => {
+  const toggleImportanceOf = (id) => {
     const note = notes.find(n => n.id === id);
     const changedNote = { ...note, important: !note.important };
 
-    noteService.update(id, changedNote)
+    noteService
+      .update(id, changedNote)
       .then(updatedNote => {
         setNotes(notes.map(note => note.id !== id ? note : updatedNote));
+      })
+      .catch(error => {
+        alert(`The note ${note.content} does not exist on server`);
+        setNotes(notes.filter(n => n.id !== note.id));
       });
   };
 
@@ -68,7 +73,7 @@ const App = () => {
           <Note
             key={note.id}
             note={note}
-            toggleImportance={() => toggleImportance(note.id)} />
+            toggleImportance={() => toggleImportanceOf(note.id)} />
         ))}
       </ul>
       <form onSubmit={addNote}>
