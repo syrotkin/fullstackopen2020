91bb0c7a471537ef58ba71b8b1815d376697b497
syrotkin <syrotkin@gmail.com>
2023-02-03 12:13:02 +0100

    use enum

--- part08/apollo-practice-01/index.js
+++ part08/apollo-practice-01/index.js
@@ -40,10 +40,15 @@ const typeDefs = gql`
         id: ID!
     }
 
+    enum YesNo {
+        YES
+        NO
+    }
+
     # has to be named Query
     type Query {
         personCount: Int!
-        allPersons: [Person!]!
+        allPersons(phone: YesNo): [Person!]!
         findPerson(name: String!): Person
     }
 
@@ -61,7 +66,17 @@ const typeDefs = gql`
 const resolvers = {
     Query: {
         personCount: () => persons.length,
-        allPersons: () => persons,
+        allPersons: (root, args) => {
+            if (!args.phone) {
+                return persons
+            }
+
+            if (args.phone === "YES") {
+                return persons.filter(p => p.phone);
+            }
+
+            return persons.filter(p => !p.phone);
+        },
         findPerson: (root, args) => persons.find(p => p.name === args.name),
     },
     
