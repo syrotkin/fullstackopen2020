5f0845a5c9c62989e1fe76a4d7e0f19b8797aad8
syrotkin <syrotkin@gmail.com>
2023-03-29 20:44:21 +0200

    add error notification for PhoneForm

--- part08/react-graphql/apollo-client/src/phoneForm.jsx
+++ part08/react-graphql/apollo-client/src/phoneForm.jsx
@@ -1,12 +1,19 @@
 import { useMutation } from "@apollo/client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { EDIT_NUMBER } from "./queries";
 
-const PhoneForm = () => {
+const PhoneForm = ({setError}) => {
     const [name, setName] = useState('');
     const [phone, setPhone] = useState('');
 
-    const [changeNumber] = useMutation(EDIT_NUMBER);
+    const [changeNumber, result] = useMutation(EDIT_NUMBER);
+
+    useEffect(() => {
+        console.log({result});
+        if(result.data && result.data.editNumber === null) {
+            setError("Person " + name + " not found");
+        }
+    }, [result.data]); // eslint-disable-line
 
     const submit = (event) => {
         event.preventDefault();
